3b4724196d74d669610232ae9381d04c1dc04ae0
Phil Cable <pcable@wsu.edu>
2018-09-13 09:29:17 -0700

    Fix front end column output
    This updates class names to avoid conflict with the Spine theme
    styles and fixes the column class output.

--- builder-templates/front-end/issue-columns.php
+++ builder-templates/front-end/issue-columns.php
@@ -70,19 +70,19 @@ $data_columns = spine_get_column_data( $ttfmake_section_data, $section_type_colu
 		if ( ! empty( $column['display-image'] ) ) {
 			if ( 'featured-image' === $column['display-image'] ) {
 				$display_image = array(
-					'class' => 'featured-image',
+					'class' => 'issue-post-featured-image',
 					'src' => get_the_post_thumbnail_url( $column['post-id'], 'spine-large_size' ),
 				);
 			} elseif ( 'thumbnail-image' === $column['display-image'] && class_exists( 'MultiPostThumbnails' ) ) {
 				$display_image = array(
-					'class' => 'thumbnail-image',
+					'class' => 'issue-post-thumbnail-image',
 					'src' => MultiPostThumbnails::get_post_thumbnail_url( 'post', 'thumbnail-image', $column['post-id'], 'spine-large_size' ),
 				);
 			}
 		}
 		?>
 
-		<article class="column <?php echo esc_attr( $column_count[ $count ] ); ?>" style="<?php echo esc_attr( $column_styles ); ?>">
+		<article class="column <?php echo esc_attr( $column_classes ); ?>"<?php if ( ! empty( $column_styles ) ) { echo ' style="' . esc_attr( $column_styles ) . '"'; } ?>>
 
 			<header>
 				<h2><a href="<?php get_permalink( get_permalink( $column['post-id'] ) ); ?>"><?php echo esc_html( $header ); ?></a></h2>
@@ -94,7 +94,7 @@ $data_columns = spine_get_column_data( $ttfmake_section_data, $section_type_colu
 			<?php } ?>
 
 			<?php if ( ! empty( $column['display-excerpt'] ) && has_excerpt( $column['post-id'] ) ) { ?>
-			<div class="excerpt">
+			<div class="issue-post-excerpt">
 				<?php echo wp_kses_post( wpautop( get_the_excerpt( $column['post-id'] ) ) ); ?>
 			</div>
 			<?php } ?>
