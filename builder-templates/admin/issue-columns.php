1641e8d870be76a967f68fcd894d2236268ab523
Phil Cable <pcable@wsu.edu>
2018-09-13 11:12:30 -0700

    Fix PHPCS violations found by Travis

--- builder-templates/admin/issue-columns.php
+++ builder-templates/admin/issue-columns.php
@@ -21,7 +21,7 @@ $j = 1;
 	foreach ( $section_order as $key => $i ) :
 
 		$column_name = $section_name . '[columns][' . $i . ']';
-		$post_id = ( isset( $ttfmake_section_data['data']['columns'][ $i ]['post-id'] ) ) ? $ttfmake_section_data['data']['columns'][ $i ]['post-id'] : '';
+		$issue_post_id = ( isset( $ttfmake_section_data['data']['columns'][ $i ]['post-id'] ) ) ? $ttfmake_section_data['data']['columns'][ $i ]['post-id'] : '';
 		$visible = ( isset( $ttfmake_section_data['data']['columns'][ $i ]['toggle'] ) ) ? $ttfmake_section_data['data']['columns'][ $i ]['toggle'] : 'visible';
 
 		if ( ! in_array( $visible, array( 'visible', 'invisible' ), true ) ) {
@@ -30,12 +30,10 @@ $j = 1;
 
 		$column_style = ( 'invisible' === $visible ) ? ' display: none;' : '';
 		$toggle_class = ( 'invisible' === $visible ) ? ' wsuwp-toggle-closed' : '';
-		//var_dump( $column_style );
-		//var_dump( $toggle_class );
 		?>
 		<div class="wsuwp-spine-builder-column wsuwp-spine-builder-column-position-<?php echo esc_attr( $j ); ?>" data-id="<?php echo esc_attr( $i ); ?>">
 			<input type="hidden" class="wsuwp-column-visible wsuwp-issue-post-meta" name="<?php echo esc_attr( $column_name ); ?>[toggle]" value="<?php echo esc_attr( $visible ); ?>"/>
-			<input type="hidden" class="wsuwp-column-post-id wsuwp-issue-post-meta" name="<?php echo esc_attr( $column_name ); ?>[post-id]" value="<?php echo esc_attr( $post_id ); ?>" />
+			<input type="hidden" class="wsuwp-column-post-id wsuwp-issue-post-meta" name="<?php echo esc_attr( $column_name ); ?>[post-id]" value="<?php echo esc_attr( $issue_post_id ); ?>" />
 			<div class="spine-builder-column-overlay">
 				<div class="spine-builder-column-overlay-wrapper">
 					<div class="spine-builder-overlay-header">
@@ -49,9 +47,9 @@ $j = 1;
 			</div>
 
 			<?php
-			if ( $post_id ) :
+			if ( $issue_post_id ) :
 				$classes = ( isset( $ttfmake_section_data['data']['columns'][ $i ]['classes'] ) ) ? $ttfmake_section_data['data']['columns'][ $i ]['classes'] : '';
-				$header = ( isset( $ttfmake_section_data['data']['columns'][ $i ]['header'] ) ) ? $ttfmake_section_data['data']['columns'][ $i ]['header'] : get_the_title( $post_id );
+				$header = ( isset( $ttfmake_section_data['data']['columns'][ $i ]['header'] ) ) ? $ttfmake_section_data['data']['columns'][ $i ]['header'] : get_the_title( $issue_post_id );
 				$subheader = ( isset( $ttfmake_section_data['data']['columns'][ $i ]['subheader'] ) ) ? $ttfmake_section_data['data']['columns'][ $i ]['subheader'] : '';
 				$display_image = ( isset( $ttfmake_section_data['data']['columns'][ $i ]['display-image'] ) ) ? $ttfmake_section_data['data']['columns'][ $i ]['display-image'] : '';
 				$display_excerpt = ( isset( $ttfmake_section_data['data']['columns'][ $i ]['display-excerpt'] ) ) ? $ttfmake_section_data['data']['columns'][ $i ]['display-excerpt'] : '';
@@ -69,7 +67,7 @@ $j = 1;
 				$post_classes .= ( 'yes' === $display_excerpt ) ? ' display-excerpt' : '';
 
 				?>
-				<div id="issue-post-<?php echo esc_attr( $post_id ); ?>"
+				<div id="issue-post-<?php echo esc_attr( $issue_post_id ); ?>"
 					class="issue-post"
 					data-classes="<?php echo esc_attr( $classes ); ?>"
 					data-header="<?php echo esc_attr( $header ); ?>"
@@ -89,7 +87,7 @@ $j = 1;
 							<div class="handlediv<?php echo esc_attr( $toggle_class ); ?>" aria-expanded="true"></div>
 						</a>
 
-						<div class="wsuwp-builder-column-title"><?php echo get_the_title( $post_id ); ?></div>
+						<div class="wsuwp-builder-column-title"><?php echo get_the_title( $issue_post_id ); ?></div>
 
 					</div>
 
@@ -101,15 +99,15 @@ $j = 1;
 						</header>
 
 						<figure class="featured-image">
-							<img src="<?php echo esc_url( get_the_post_thumbnail_url( $post_id, 'spine-large_size' ) ); ?>" />
+							<img src="<?php echo esc_url( get_the_post_thumbnail_url( $issue_post_id, 'spine-large_size' ) ); ?>" />
 						</figure>
 
 						<figure class="thumbnail-image">
-							<img src="<?php echo esc_url( MultiPostThumbnails::get_post_thumbnail_url( 'post', 'thumbnail-image', $post_id, 'spine-large_size' ) ); ?>" />
+							<img src="<?php echo esc_url( MultiPostThumbnails::get_post_thumbnail_url( 'post', 'thumbnail-image', $issue_post_id, 'spine-large_size' ) ); ?>" />
 						</figure>
 
 						<div class="excerpt">
-							<?php if ( has_excerpt( $post_id ) ) { echo wp_kses_post( wpautop( get_the_excerpt( $post_id ) ) ); } ?>
+							<?php if ( has_excerpt( $issue_post_id ) ) { echo wp_kses_post( wpautop( get_the_excerpt( $issue_post_id ) ) ); } ?>
 						</div>
 
 					</article>
